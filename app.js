d57c623d16a8114fc039d2b1bd9dfd7e0a838bbb
SombreroElGringo <florent.pailhes@hotmail.fr>
2017-10-13 17:02:43 +0200

    Server + Config
    * Express server
    * Express config
    * Env file
    * Mongoose

---
+++ app.js
@@ -0,0 +1,55 @@
+/**
+ *  Modules dependencies
+ */
+const express = require('express');
+const chalk = require('chalk');
+const errorHandler = require('errorhandler');
+const dotenv = require('dotenv');
+const path = require('path');
+const mongoose = require('mongoose');
+
+/**
+ *  Load environment variables from .env file, where API keys and passwords are configured.
+ */
+dotenv.load({path: '.env.secret'});
+
+/**
+ *  Create Express Server
+ */
+const app = express();
+
+/**
+ * Connect to MongoDB.
+ */
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+  process.exit();
+});
+
+/**
+ * Express configuration.
+ */
+module.exports = require('./config/express')(app, process);
+
+/**
+ * App routes.
+ */
+module.exports = require('./app/routes')(app);
+
+/**
+ * Error Handler.
+ */
+app.use(errorHandler());
+
+/**
+ * Start Express server.
+ */
+app.listen(app.get('port'), () => {
+  console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env')); 
+  console.log('-- Press CTRL-C to stop --\n');
+});
+
+module.exports = app;
---
+++ config/express.js
@@ -0,0 +1,63 @@
+/**
+ * Module dependencies.
+ */
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const lusca = require('lusca');
+const path = require('path');
+const expressValidator = require('express-validator');
+const expressStatusMonitor = require('express-status-monitor');
+const sass = require('node-sass-middleware');
+
+module.exports = function(app, config) {
+   app.set('port', process.env.PORT || 3000);
+   app.set('views', path.join(__dirname, '../app/views'));
+   app.set('view engine', 'pug');
+   app.use(expressStatusMonitor());
+   app.use(sass({
+     src: path.join(__dirname, '../public'),
+     dest: path.join(__dirname, '../public')
+   }));
+   app.use(logger('dev'));
+   app.use(bodyParser.json());
+   app.use(bodyParser.urlencoded({ extended: true }));
+   app.use(expressValidator());
+   app.set('trust proxy', 1)
+   app.use(session({
+       secret: 'TD T0D0 L15T',
+       resave: false,
+       saveUninitialized: true,
+       cookie: { secure: false }
+   }))
+   app.use((req, res, next) => {
+       if (req.path === '/api/upload') {
+           next();
+       } else {
+           lusca.csrf()(req, res, next);
+       }
+   });
+   app.use(lusca.xframe('SAMEORIGIN'));
+   app.use(lusca.xssProtection(true));
+   app.use((req, res, next) => {
+       res.locals.user = req.user;
+       next();
+   });
+   app.use((req, res, next) => {
+       // After successful login, redirect back to the intended page
+       if (!req.user &&
+           req.path !== '/login' &&
+           req.path !== '/signup' &&
+           !req.path.match(/^\/auth/) &&
+           !req.path.match(/\./)) {
+           req.session.returnTo = req.path;
+       } else if (req.user &&
+           req.path == '/account') {
+           req.session.returnTo = req.path;
+       }
+       next();
+   });
+   app.use(express.static(path.join(__dirname, '../public'), { maxAge: 31557600000 }));
+
+   return app;
+}
